746eed0420e75e0a62a64e3c445a2da950515925
LoganLevitre <45443105+LoganBEary@users.noreply.github.com>
2021-04-13 09:25:30 -0700

    Added components

--- web-scrap/web-scrapper/src/App.js
+++ web-scrap/web-scrapper/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './Components/header.js'
+import Header from './Components/header.js';
+import Pop_up from './Components/pop_up.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Pop_up/>
     </div>
   );
 }
