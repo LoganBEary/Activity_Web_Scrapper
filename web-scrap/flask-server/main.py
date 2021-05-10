04394d5429e0b78738e5a6f4c57f3fb0adf4cb11
LoganLevitre <45443105+LoganBEary@users.noreply.github.com>
2021-05-10 00:37:22 -0700

    connected react to Flask

--- web-scrap/flask-server/main.py
+++ web-scrap/flask-server/main.py
@@ -1,7 +1,9 @@
-from flask import (Flask, rentder_template)
+from flask import (Flask, render_template)
 
 app = Flask("__main__")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",token="Hello")
+
+app.run(debug=True)
